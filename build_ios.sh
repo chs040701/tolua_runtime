74ed8310c365cf0049fd9cc87a0b25da51cdeb27
alex4814 <alex4814@126.com>
2025-01-29 20:35:30 +0800

    build(ios): build script and Xcode project modifications for tolua library on iOS using LuaJIT

--- build_ios.sh
+++ build_ios.sh
@@ -3,48 +3,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LIPO="xcrun -sdk iphoneos lipo"
 STRIP="xcrun -sdk iphoneos strip"
 
-SRCDIR=$DIR/luajit-2.1/
+SRCDIR=$DIR/luajit-2.1/src
 DESTDIR=$DIR/iOS
-IXCODE=`xcode-select -print-path`
-ISDK=$IXCODE/Platforms/iPhoneOS.platform/Developer
-ISDKD=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/
-ISDKVER=iPhoneOS.sdk
-ISDKP=$IXCODE/usr/bin/
-
-if [ ! -e $ISDKP/ar ]; then 
-  sudo cp $ISDKD/usr/bin/ar $ISDKP
-fi
-
-if [ ! -e $ISDKP/ranlib ]; then
-  sudo cp $ISDKD/usr/bin/ranlib $ISDKP
-fi
-
-if [ ! -e $ISDKP/strip ]; then
-  sudo cp $ISDKD/usr/bin/strip $ISDKP
-fi
 
 rm "$DESTDIR"/*.a
-cd $SRCDIR
 
-make clean
-ISDKF="-arch armv7 -isysroot $ISDK/SDKs/$ISDKVER -miphoneos-version-min=8.0 -fembed-bitcode"
-make HOST_CC="gcc -m32" TARGET_FLAGS="$ISDKF" TARGET=armv7 TARGET_SYS=iOS BUILDMODE=static
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7.a
+# iOS/ARM64
+ISDKP=$(xcrun --sdk iphoneos --show-sdk-path)
+ICC=$(xcrun --sdk iphoneos --find clang)
+ISDKF="-arch arm64 -isysroot $ISDKP -miphoneos-version-min=8.0 -fembed-bitcode"
+make -C $SRCDIR DEFAULT_CC=clang CROSS="$(dirname $ICC)/" TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS BUILDMODE=static clean
+make -C $SRCDIR DEFAULT_CC=clang CROSS="$(dirname $ICC)/" TARGET_FLAGS="$ISDKF" TARGET_SYS=iOS BUILDMODE=static
+mv "$SRCDIR"/libluajit.a "$DESTDIR"/libluajit.a
 
-make clean
-ISDKF="-arch armv7s -isysroot $ISDK/SDKs/$ISDKVER -miphoneos-version-min=8.0 -fembed-bitcode"
-make HOST_CC="gcc -m32" TARGET_FLAGS="$ISDKF" TARGET=armv7s TARGET_SYS=iOS BUILDMODE=static 
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-armv7s.a
+cd "$DESTDIR"
 
-make clean
-ISDKF="-arch arm64 -isysroot $ISDK/SDKs/$ISDKVER -miphoneos-version-min=8.0 -fembed-bitcode"
-make HOST_CC="gcc " TARGET_FLAGS="$ISDKF" TARGET=arm64 TARGET_SYS=iOS BUILDMODE=static
-mv "$SRCDIR"/src/libluajit.a "$DESTDIR"/libluajit-arm64.a
-make clean
-
-cd ../iOS
-$LIPO -create "$DESTDIR"/libluajit-*.a -output "$DESTDIR"/libluajit.a
-$STRIP -S "$DESTDIR"/libluajit.a
 xcodebuild clean
 xcodebuild -configuration=Release
 cp -f ./build/Release-iphoneos/libtolua.a ../Plugins/iOS/
+
