00bbafe089335e36d41138cd3c0a0901bb7b52bd
alex4814 <alex4814@126.com>
2025-02-06 17:46:50 +0800

    build(win32): convert to CMake build system
    - using MSVC to build LuaJIT on Windows
    - removed protobuf

---
+++ make_luajit2_windows_x86.bat
@@ -0,0 +1,34 @@
+@rem Script to build LuaJIT with MSVC using provided `msvcbuild.bat`.
+@rem Copyright (C) 2025 Rython Fu.
+@rem
+@rem Use the following options, if needed. The default is a static release build.
+@rem
+@rem   debug    whether emit debug symbols
+
+@echo off
+setlocal enabledelayedexpansion
+
+@if "%1" neq "debug" goto :NODEBUG
+set MSVCBUILD_ARGS=debug static
+:NODEBUG
+set MSVCBUILD_ARGS=static
+
+set VSWHERE_PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+if not exist "%VSWHERE_PATH%" (
+    echo Error: vswhere.exe not found. Ensure Visual Studio 2017+ is installed.
+    exit /b 1
+)
+
+for /f "usebackq tokens=*" %%i in (`"%VSWHERE_PATH%" -latest -property installationPath`) do (
+    set VS_PATH=%%i
+)
+if not defined VS_PATH (
+    echo Error: Visual Studio installation not found.
+    exit /b 1
+)
+
+set LUAJIT_SOURCE_DIR=%~dp0\luajit-2.1\src
+call "%VS_PATH%\VC\Auxiliary\Build\vcvarsall.bat" x86 && (
+    cd /d "%LUAJIT_SOURCE_DIR%"
+    call msvcbuild.bat %MSVCBUILD_ARGS%
+)
